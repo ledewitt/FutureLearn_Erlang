a0039c043cdf617404f9e54ec0c63b71266b1230
Luke DeWitt <luke@dewittsoft.com>
2017-02-22 13:42:57 -0700

    Commiting with new work from Week1 Section15.

---
+++ Week1/Step_1_15/patterns.erl
@@ -0,0 +1,12 @@
+-module(patterns).
+-export([maxThree/3,
+         howManyEqual/3]).
+
+maxThree(X,Y,Z) ->
+  max(max(X,Y),Z).
+
+howManyEqual(X,X,X) -> 3;
+howManyEqual(X,X,_) -> 2;
+howManyEqual(X,_,X) -> 2;
+howManyEqual(_,X,X) -> 2;
+howManyEqual(_,_,_) -> 0.
