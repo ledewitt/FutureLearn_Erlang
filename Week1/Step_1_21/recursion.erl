347bbb9ea142a28cbb02892e35169d6d1cf99097
Luke DeWitt <luke@dewittsoft.com>
2017-02-24 13:19:41 -0700

    Working on the presentation material for Step.1.21

---
+++ Week1/Step_1_21/recursion.erl
@@ -0,0 +1,50 @@
+-module(recursion).
+-export([fac/1,
+         loop/1,
+         sum/1
+        ]).
+
+% Work from 1_21
+
+fac(N) -> fac(N,1).
+
+fac(0,P) -> P;
+fac(N,P) when N > 0 -> fac(N-1,P*N).
+
+% Sum work Example asked to work in presentation
+
+sum(N) -> sum(N,0).
+
+sum(0,P) -> P;
+sum(N,P) when N > 0 -> sum(N-1, P+N).
+
+
+% Looping the loop
+
+loop(N) when N>0 ->
+  io:format("~p~n",[N]),
+  loop(N-1);
+loop(0) ->
+  io:format("bye~n").
+
+% Work from 1_19
+%
+% fac(0) -> 1;
+% fac(N) when N>0 -> fac(N-1)*N.
+% 
+% fib(0) -> 0;
+% fib(1) -> 1;
+% fib(N) when N>0 -> fib(N-1)+fib(N-2).
+
+% pieces(0) -> 1;
+% pieces(N) when N>0 -> N + pieces(N-1).
+
+% binomial(N,K) -> fac(N)/(fac(K)*fac(N-K)).
+
+% thirdDimPieces(0) -> 1;
+% thirdDimPieces(1) -> 2;
+% thirdDimPieces(2) -> 4;
+% thirdDimPieces(N) when N>3 -> binomial(N,3)
+%                               + binomial(N,2)
+%                               + binomial(N,1)
+%                               + binomial(N,0).
